949c073e998fa7a9f5e396e79884a3d8842c9669
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-30 22:52:35 +0530

    add forEach loop

---
+++ array/ForEach.java
@@ -0,0 +1,10 @@
+package array;
+
+public class ForEach {
+    static void main(String[] args) {
+        int[] arr = {5,6,7,4,3,9};
+        for(int ele : arr){
+            System.out.println(ele);        // forEach loop use kiya to array per operations nhi ho sakate sirf array ke elements print hote hai
+        }
+    }
+}
