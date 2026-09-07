1951f6c71be7c63119729e03506e72fbf7b57210
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-07 23:34:15 +0530

    print even num

--- array/QueForLoop.java
+++ array/QueForLoop.java
@@ -13,6 +13,11 @@ public class QueForLoop {
             }
 
             // Print even numbers 1 to 50
+            for(int i = 1; i <= 50; i++) {
+                if(i % 2 == 0) {
+                    System.out.println(i);
+                }
+            }
 
         }
     }
