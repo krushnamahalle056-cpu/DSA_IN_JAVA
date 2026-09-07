48d7570e09a6386081f2bb84eee905bce308e361
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-07 23:37:42 +0530

    add new que

--- array/QueForLoop.java
+++ array/QueForLoop.java
@@ -26,6 +26,16 @@ public class QueForLoop {
                 }
             }
 
+            //Sum of 1 to 100
+            int sum = 0;
+
+            for(int i = 1; i <= 100; i++) {
+                sum = sum + i;
+            }
+
+            System.out.println(sum);
+        }
+
         }
     }
 }
