8cef4600d999e712874160573aca87eef646014a
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-07 23:33:28 +0530

    new q

--- array/QueForLoop.java
+++ array/QueForLoop.java
@@ -6,6 +6,14 @@ public class QueForLoop {
             for(int i = 1; i <= 20; i++) {
                 System.out.println(i);
             }
+
+            // Print 20 to 1
+            for(int i = 20; i >= 1; i--) {
+                System.out.println(i);
+            }
+
+            // Print even numbers 1 to 50
+
         }
     }
 }
