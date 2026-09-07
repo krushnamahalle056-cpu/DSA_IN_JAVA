e2540cdf263f146345ff2102f28485aaf7f7357b
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-07 23:35:59 +0530

    odd num code add

--- array/QueForLoop.java
+++ array/QueForLoop.java
@@ -19,6 +19,13 @@ public class QueForLoop {
                 }
             }
 
+            //Print odd numbers 1 to 50
+            for(int i = 1; i <= 50; i++) {
+                if(i % 2 != 0) {
+                    System.out.println(i);
+                }
+            }
+
         }
     }
 }
