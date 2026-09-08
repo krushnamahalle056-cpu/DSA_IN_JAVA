afcb6165a28cebb5417aa360c52df8c73ad47007
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-08 22:18:44 +0530

    add new code

--- array/MergeTwoSortedArrays.java
+++ array/MergeTwoSortedArrays.java
@@ -1,43 +1,36 @@
 package array;
 
+
 import static java.io.ObjectInputFilter.merge;
 
 public class MergeTwoSortedArrays {
     static void main(String[] args) {
         int[] a = {1,3,5,7,8};
         int[] b = {2,4,6,9,10,22,24};
-        int[] c = new int[a.length+b.length];
-        merge(c,a,b);
+        int[] c = new int[a.length + b.length];
+        for(int ele : c) System.out.print(ele + " ");
+        System.out.println();
+        marge(c,a,b);
+        for(int ele : c) System.out.print(ele + " ");
 
 
     }
 
-    public static void merge(int[] a,int[] b,int[] c){
+    public static void marge(int[] c,int[] a,int[] b){
         int i=0,j=0,k=0;
         while(i<a.length && j<b.length){
             if(a[i]<b[j]){
-                c[k] =  a[i];
-                i++;
-                k++;
-            }else if(a[i]>b[j]){
-                c[k] =  b[j];
-                j++;
-                k++;
-            }
+                c[k++] =  a[i++];
+            }else c[k++] =  b[j++];
         }
-        if(i==a.length){  // a array khatam -> b ke bache hue ele lo
-            while(j<b.length){
-                c[k] =  b[j];
-                j++;
-                k++;
-            }
+
+        while(j<b.length){
+            c[k++] =  b[j++];
         }
-        if(j==b.length){  // b array khatam -> a ke bache hue ele lo
-            while(i<a.length){
-                c[k] =  b[i];
-                i++;
-                k++;
-            }
+
+        while(i<a.length){
+                c[k++] =  b[i++];
         }
+
     }
 }
