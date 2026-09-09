8551bb918453462e6ce8facca304afd10ababb9f
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-09 18:15:38 +0530

    sum of array que

--- array/FindDoublicateEle.java
+++ array/FindDoublicateEle.java
@@ -8,6 +8,13 @@ public class FindDoublicateEle {
             System.out.print(ele+" ");
         }
 
+        // Que: Find sum of array
+        int[] a = {10, 20, 30, 40, 50};
+        int sum = 0;
+        for(int i=0; i<a.length; i++){
+            sum += a[i];
+        }
+        System.out.print("sum= "+sum);
 
     }
 }
