911970e2be3659e481c2c7e6bba3befcb98265b7
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-09 18:14:50 +0530

    add que

---
+++ array/FindDoublicateEle.java
@@ -0,0 +1,13 @@
+package array;
+
+public class FindDoublicateEle {
+    static void main(String[] args) {
+       // Que: Print all elements
+        int[] arr = {10, 20, 30, 40, 50};
+        for(int ele: arr){
+            System.out.print(ele+" ");
+        }
+
+
+    }
+}
