83c79fa5626dc356d071ccc399f07a900cab37df
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-12 15:51:32 +0530

    add print fun

--- Basic_Sorting/Bubble_Sort.java
+++ Basic_Sorting/Bubble_Sort.java
@@ -1,8 +1,14 @@
 package Basic_Sorting;
 
 public class Bubble_Sort {
+    public static void print(int[] arr){
+        for(int ele : arr){
+            System.out.print(ele);
+        }
+    }
     static void main(String[] args) {
         int[] arr = {2,4,5,3,1,7,6};
+
         for(int i = 0; i<arr.length-1; i++){
             if(arr[i] > arr[i+1]){
                 int temp = arr[i];
