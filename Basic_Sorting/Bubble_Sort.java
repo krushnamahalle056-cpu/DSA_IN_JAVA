97f0530ff913f0d53bc8bd8a775cd3081735786f
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-12 16:28:28 +0530

    add comment

--- Basic_Sorting/Bubble_Sort.java
+++ Basic_Sorting/Bubble_Sort.java
@@ -11,16 +11,19 @@ public class Bubble_Sort {
         int n = arr.length;
         print(arr);
 
-       for(int i =0; i<n-1; i++){
-           for(int j = 0; j<n-1-i; j++){       // condition j<n-1-i becouse last ka element first round main hi  ---
-               if(arr[j] > arr[j+1]){          // sort ho jayega to hame loop n-1 tak chalane ki jarurat nhi hai ---
-                   int temp = arr[j];          // isse time complexity kam ho jayegi .
-                   arr[j] = arr[j+1];
-                   arr[j+1] = temp;
+//       for(int i =0; i<n-1; i++){
+//           for(int j = 0; j<n-1-i; j++){       // condition j<n-1-i becouse last ka element first round main hi  ---
+//               if(arr[j] > arr[j+1]){          // sort ho jayega to hame loop n-1 tak chalane ki jarurat nhi hai ---
+//                   int temp = arr[j];          // isse time complexity kam ho jayegi .
+//                   arr[j] = arr[j+1];
+//                   arr[j+1] = temp;
+//
+//               }
+//           }
+//       }
 
-               }
-           }
-       }
+
+
         System.out.println(" ");
        print(arr);
 
