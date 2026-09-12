928087c818f6839188303fb9f820a85cf5102097
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-12 16:11:26 +0530

    reduce time complexity

--- Basic_Sorting/Bubble_Sort.java
+++ Basic_Sorting/Bubble_Sort.java
@@ -11,12 +11,12 @@ public class Bubble_Sort {
         int n = arr.length;
         print(arr);
 
-       for(int j =1; j<n-1; j++){
-           for(int i = 0; i<n-1; i++){
-               if(arr[i] > arr[i+1]){
-                   int temp = arr[i];
-                   arr[i] = arr[i+1];
-                   arr[i+1] = temp;
+       for(int i =0; i<n-1; i++){
+           for(int j = 0; j<n-1-i; j++){       // condition j<n-1-i becouse last ka element first round main hi  ---
+               if(arr[j] > arr[j+1]){          // sort ho jayega to hame loop n-1 tak chalane ki jarurat nhi hai ---
+                   int temp = arr[j];          // isse time complexity kam ho jayegi .
+                   arr[j] = arr[j+1];
+                   arr[j+1] = temp;
 
                }
            }
