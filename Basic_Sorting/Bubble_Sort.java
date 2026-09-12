ad3d514e4a521f596f59fb8bd5f7253aed53f6b5
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-12 15:49:43 +0530

    Basic_Sorting code add

--- Basic_Sorting/Bubble_Sort.java
+++ Basic_Sorting/Bubble_Sort.java
@@ -1,4 +1,16 @@
 package Basic_Sorting;
 
 public class Bubble_Sort {
+    static void main(String[] args) {
+        int[] arr = {2,4,5,3,1,7,6};
+        for(int i = 0; i<arr.length-1; i++){
+            if(arr[i] > arr[i+1]){
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+
+            }
+        }
+
+    }
 }
