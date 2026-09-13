2905b8ee5e386f6fc3ae0fd9bd980aedd76fb2a3
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-13 23:08:38 +0530

    add print function

--- Basic_Sorting/insertion_Sort.java
+++ Basic_Sorting/insertion_Sort.java
@@ -1,5 +1,10 @@
 package Basic_Sorting;
 
 public class insertion_Sort {
+    public static void print(int[] arr){
+        for(int ele: arr){
+            System.out.print(ele + " ");
+        }
+    }
 
 }
