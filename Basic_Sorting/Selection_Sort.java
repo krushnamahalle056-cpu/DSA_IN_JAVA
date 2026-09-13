9fc754b2b6eb014537ce7a82acbf5982e55f8069
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-13 23:01:43 +0530

    selection sort add

---
+++ Basic_Sorting/Selection_Sort.java
@@ -0,0 +1,12 @@
+package Basic_Sorting;
+
+public class Selection_Sort {
+    public static void print(int[] arr){
+        for(int ele: arr){
+            System.out.print(ele + " ");
+        }
+    }
+
+
+
+}
