da3194604927eca83d9cd134204929249b74a9ef
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-14 11:01:16 +0530

    changes add on incertion sort

--- Basic_Sorting/insertion_Sort.java
+++ Basic_Sorting/insertion_Sort.java
@@ -8,12 +8,12 @@ public class insertion_Sort {
     }
 
     static void main(String[] args) {
-        int[] arr = {3,5,-1,0,2,8,5};
+        int[] arr = {3,5,-1,0,2,-8,5,-55,0,3,32};
         print(arr);
 
         for(int i = 1; i< arr.length; i++){
             int j = i;
-            while(j > 0 && arr[j] > arr[j-1]){
+            while(j > 0 && arr[j] < arr[j-1]){
                 int temp = arr[j] ;
                 arr[j] = arr[j-1];
                 arr[j-1] = temp;
