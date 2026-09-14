9c776c7d335a99e5a7c3da1ab5c15c84bdb07784
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-14 10:17:12 +0530

    print fun add

--- Basic_Sorting/Kth_Smallest_Que.java
+++ Basic_Sorting/Kth_Smallest_Que.java
@@ -1,6 +1,11 @@
 package Basic_Sorting;
 
 public class Kth_Smallest_Que {
+    public static void print(int[] arr ){
+        for(int ele : arr){
+            System.out.print(ele + " ");
+        }
+    }
     static void main(String[] args) {
         int[] arr = {10, 5, 4, 3, 48, 6, 2, 33, 53, 10};
         int k = 4;
