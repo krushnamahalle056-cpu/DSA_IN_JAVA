2945bad22a17ea9f34ef8bc5a75dc8d42e7f4ddb
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-14 10:58:03 +0530

    add incertion sort

--- Basic_Sorting/insertion_Sort.java
+++ Basic_Sorting/insertion_Sort.java
@@ -11,6 +11,21 @@ public class insertion_Sort {
         int[] arr = {3,5,-1,0,2,8,5};
         print(arr);
 
+        for(int i = 1; i< arr.length; i++){
+            int j = i;
+            while(j > 0 && arr[j] > arr[j-1]){
+                int temp = arr[j] ;
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+                j--;
+
+            }
+        }
+
+        System.out.println(" ");
+
+        print(arr);
+
     }
 
 }
