599676c2cabef36f28aaba388d48447d28ae2b8b
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-14 10:15:10 +0530

    add code

--- Basic_Sorting/Kth_Smallest_Que.java
+++ Basic_Sorting/Kth_Smallest_Que.java
@@ -5,6 +5,18 @@ public class Kth_Smallest_Que {
         int[] arr = {10, 5, 4, 3, 48, 6, 2, 33, 53, 10};
         int k = 4;
         int n = arr.length;
+        for(int i = 0; i<k ; i++){
+            int minIndex = i;
+            for(int j = i+1; j<n; j++){
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
+
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex] ;
+            arr[minIndex] = temp;
+        }
 
     }
 }
