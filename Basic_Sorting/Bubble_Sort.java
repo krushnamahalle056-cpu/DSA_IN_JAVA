c6df1da54cdd9a8e56697459dea424baab3d87c6
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-14 10:14:29 +0530

    add kth smallest ele que using selection_sort

--- Basic_Sorting/Bubble_Sort.java
+++ Basic_Sorting/Bubble_Sort.java
@@ -26,7 +26,7 @@ public class Bubble_Sort {
         for(int i =0; i<n-1; i++){
             int swap = 0;
             for(int j = 0; j<n-1-i; j++){
-                if(arr[j] >   arr[j+1]){
+                if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
---
+++ Basic_Sorting/Kth_Smallest_Que.java
@@ -0,0 +1,10 @@
+package Basic_Sorting;
+
+public class Kth_Smallest_Que {
+    static void main(String[] args) {
+        int[] arr = {10, 5, 4, 3, 48, 6, 2, 33, 53, 10};
+        int k = 4;
+        int n = arr.length;
+
+    }
+}
