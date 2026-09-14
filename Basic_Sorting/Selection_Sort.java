4cc20e6dbf4379bed0d6ea3fb3b25d9c40882072
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-14 09:21:05 +0530

    code as comment add

--- Basic_Sorting/Selection_Sort.java
+++ Basic_Sorting/Selection_Sort.java
@@ -11,20 +11,22 @@ public class Selection_Sort {
         int[] arr = {1,-2,6,0,5,8,4,7};
         int n = arr.length;
         print(arr);
-        for(int i = 0; i<n-1; i++){
-            int min = Integer.MAX_VALUE;
-            int minIndex = -1;
-            for(int j =i; j<n; j++){
-                if (arr[j] < min){
-                    min = arr[j];
-                    minIndex = j;
-                }
-            }
 
-            int temp = arr[i];
-            arr[i] = arr[minIndex] ;
-            arr[minIndex] = temp;
-        }
+
+//        for(int i = 0; i<n-1; i++){
+//            int min = Integer.MAX_VALUE;
+//            int minIndex = -1;
+//            for(int j =i; j<n; j++){
+//                if (arr[j] < min){
+//                    min = arr[j];
+//                    minIndex = j;
+//                }
+//            }
+//
+//            int temp = arr[i];
+//            arr[i] = arr[minIndex] ;
+//            arr[minIndex] = temp;
+//        }
         System.out.println(" ");
         print(arr);
     }
