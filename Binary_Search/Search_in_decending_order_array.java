46a7b827b5c7675db08f670fa9690cfe3508ebb0
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-15 16:23:02 +0530

    code for binary search and array is desending order

---
+++ Binary_Search/Search_in_decending_order_array.java
@@ -0,0 +1,17 @@
+package Binary_Search;
+
+public class Search_in_decending_order_array {
+    static void main(String[] args) {
+        int[] arr = {99,97,86,76,65,43,21,8,6,2,1};
+        int n = arr.length;
+        int target = 8;
+        int start = 0 ;
+        int end = n-1;
+        int index= -1;
+
+
+        System.out.println(index);
+
+    }
+
+}
