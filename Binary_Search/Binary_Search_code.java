3805a7b9a672996d0f0ab09dfaf7c2bca5a20647
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-15 11:04:49 +0530

    initialize the variables

--- Binary_Search/Binary_Search_code.java
+++ Binary_Search/Binary_Search_code.java
@@ -2,6 +2,11 @@ package Binary_Search;
 
 public class Binary_Search_code {
     static void main(String[] args) {
+        int[] arr = {-1,-3,3,5,7,8,9,12,14,19,23,34,56,78,909};
+        int target = 23;
+        int n = arr.length;
+        int start = 0;
+        int end = n-1;
 
     }
 }
