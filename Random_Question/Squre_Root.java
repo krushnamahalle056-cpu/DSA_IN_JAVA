2952b243cda215a2f0e188f91df72800229bb17f
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-15 21:35:50 +0530

    add square root code

---
+++ Random_Question/Squre_Root.java
@@ -0,0 +1,17 @@
+package Random_Question;
+
+public class Squre_Root {
+    static void main(String[] args) {
+        int n = 81;
+        int root = 0;
+        for(int i =1; i<=n; i++){
+
+            if(i*i>n){
+                break;
+            }
+            root = i;
+        }
+        System.out.println(root);
+    }
+}
+
