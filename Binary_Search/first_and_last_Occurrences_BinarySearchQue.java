1afac5c6e04f86cc1a5d0a754c6ee32414427af0
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-15 11:38:08 +0530

    initialize the variables

--- Binary_Search/first_and_last_Occurrences_BinarySearchQue.java
+++ Binary_Search/first_and_last_Occurrences_BinarySearchQue.java
@@ -1,7 +1,18 @@
 package Binary_Search;
 
+import java.util.ArrayList;
+
 public class first_and_last_Occurrences_BinarySearchQue {
+
     static void main(String[] args) {
+        int[] arr = {-3,1,2,5,5,5,5,5,5,5,7,8,9,12,14,19,23,34,56,78,909};
+        ArrayList<Integer> newarr= new ArrayList<>();
+        int n = arr.length;
+        int x= 5;
+        int start = 0;
+        int end = n-1;
+        int indx1=-1;
+
 
     }
 }
