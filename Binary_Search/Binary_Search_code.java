d9e0297f3abf4f63cd41673e48e05a9c7775f36a
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-15 11:06:14 +0530

    initial commit

--- Binary_Search/Binary_Search_code.java
+++ Binary_Search/Binary_Search_code.java
@@ -7,6 +7,16 @@ public class Binary_Search_code {
         int n = arr.length;
         int start = 0;
         int end = n-1;
-
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(arr[mid] == target){
+                System.out.println(mid);
+            }else if(arr[mid] < target){
+                start = mid +1;
+            }
+            else{
+                end = mid -1;
+            }
+        }
     }
 }
