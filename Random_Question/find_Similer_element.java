1ae50bf40c2b7bbef4b53aca89f57a8da8046e1f
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-16 22:27:04 +0530

    initialize the variable

--- Random_Question/find_Similer_element.java
+++ Random_Question/find_Similer_element.java
@@ -2,6 +2,10 @@ package Random_Question;
 
 public class find_Similer_element {
     static void main(String[] args) {
+        int[] arr= {1,1,2,2,4,4,6,6,7,8,8,9,9};
+        int n = arr.length;
+        int start = 0 ;
+        int end = n-1;
 
     }
 }
