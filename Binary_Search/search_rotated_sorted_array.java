a55edff3dd100eda033083a58ba473372d18d22d
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-17 18:05:53 +0530

    logit add for half sorted array

--- Binary_Search/search_rotated_sorted_array.java
+++ Binary_Search/search_rotated_sorted_array.java
@@ -3,5 +3,16 @@ package Binary_Search;
 public class search_rotated_sorted_array {
     static void main(String[] args) {
         int[] arr = {3,5,6,7,8,9,10,1,2};
+        int target = 1;
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[start] <= arr[mid]){
+                if(arr[start] >= target && arr[mid] <= target){
+                    start = mid+1;
+                }
+            }
+        }
     }
 }
