dd4afd2ff6e94b1ad043ae3bf070eb8b6d3dcb55
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-24 01:00:29 +0530

    print the right angle triangle

--- array/new_que.java
+++ array/new_que.java
@@ -37,8 +37,8 @@ public class new_que {
             System.out.print(newArray[j] + " ");
         }
 
-        for(int l=0; l<5; l++){
-            for(int k = 0; k<5 ; k++){
+        for(int l=5; l>0; l--){
+            for(int k =5; k>l-1; k--){
                 System.out.print("*");
             }
             System.out.println(" ");
