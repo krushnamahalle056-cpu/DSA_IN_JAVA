be6dccb8afd5965f07c65a84d6bb2f704b4b32f2
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-23 23:46:19 +0530

    add random array

--- Binary_Search/Count_Max_NegativeInt_PositiveInt.java
+++ Binary_Search/Count_Max_NegativeInt_PositiveInt.java
@@ -4,6 +4,6 @@ public class Count_Max_NegativeInt_PositiveInt {
     static void main(String[] args) {
         int[] arr = {-2,-3,-4,-5,-7,-8,-9,0,0,2,4,5,6,7,8,9};
         int n = arr.length;
-
+        
     }
 }
---
+++ array/new_que.java
@@ -0,0 +1,9 @@
+package array;
+
+public class new_que {
+    static void main(String[] args) {
+        int[] arr = {2,4,5,6,7,86,55,34,32,22,11};
+        int n = arr.length;
+
+    }
+}
