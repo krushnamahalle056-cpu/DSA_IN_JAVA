29cfa6c958e0564837d8fba0497679c0d2a4e5f6
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-23 23:57:47 +0530

    rearrange code

--- array/new_que.java
+++ array/new_que.java
@@ -4,15 +4,16 @@ public class new_que {
     static void main(String[] args) {
         int[] arr = {2,4,5,6,7,86,55,34,32,22,11};
         int n = arr.length;
+
         // Print the array
         for(int i=0;i<n; i++){
-            System.out.print(i+" "+arr[i]);
+            System.out.print(arr[i] + " ");
         }
 
         // Second method to print array
         int i = 0;
         while(i<n){
-            System.out.println(i+" "+arr[i]);
+            System.out.println(arr[i] + " ");
             i++;
         }
 
