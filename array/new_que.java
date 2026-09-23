f8f55158b39c46a2f8b97bcdf3dfb6877c5e702e
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-23 23:47:53 +0530

    print array

--- array/new_que.java
+++ array/new_que.java
@@ -4,6 +4,9 @@ public class new_que {
     static void main(String[] args) {
         int[] arr = {2,4,5,6,7,86,55,34,32,22,11};
         int n = arr.length;
+        for(int i=0;i<n; i++){
+            System.out.println(i+" "+arr[i]);
+        }
 
     }
 }
