5db93ef6431baaafaef5b102c6c8477df6ce403d
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-24 15:38:35 +0530

    input string input

--- Strings/StringBasics.java
+++ Strings/StringBasics.java
@@ -1,5 +1,7 @@
 package Strings;
 
+import java.util.Scanner;
+
 public class StringBasics {
     static void main(String[] args) {
 //        char[] arr = {'a', 'd', 'c'};
@@ -7,8 +9,16 @@ public class StringBasics {
 //            System.out.print(ele +" ");
 //        }
 
-        // Declaration of String
-        String str = "Hello Krushna";
-        System.out.println(str);
+//        // Declaration of String
+//        String str = "Hello Krushna";
+//        System.out.println(str);
+//
+        // input string from user
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your full Name : ");
+        String str = sc.next();         // sc.next() gives the first word of full sentence
+        System.out.println("Your full Name is: " + str);
+
+
     }
 }
