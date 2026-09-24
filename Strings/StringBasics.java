7891cdbda3f5db9933e43c1f9f1003f7a308af54
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-24 15:18:57 +0530

    declaration of string

--- Strings/StringBasics.java
+++ Strings/StringBasics.java
@@ -2,9 +2,13 @@ package Strings;
 
 public class StringBasics {
     static void main(String[] args) {
-        char[] arr = {'a', 'd', 'c'};
-        for(char ele: arr){
-            System.out.print(ele +" ");
-        }
+//        char[] arr = {'a', 'd', 'c'};
+//        for(char ele: arr){
+//            System.out.print(ele +" ");
+//        }
+
+        // Declaration of String
+        String str = "Hello Krushna";
+        System.out.println(str);
     }
 }
