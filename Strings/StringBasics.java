33c1c913665f3187fdf96fa017d81c9a2affbf78
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-24 15:13:37 +0530

    new topic string start

---
+++ Strings/StringBasics.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class StringBasics {
+    static void main(String[] args) {
+        char[] arr = {'a','d','c'};
+        for(char ele: arr){
+            System.out.print(ele);
+        }
+    }
+}
