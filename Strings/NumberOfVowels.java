a71e97f1e24a3c694760181bd756cd283de72070
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-25 09:52:39 +0530

    enter string

--- Strings/NumberOfVowels.java
+++ Strings/NumberOfVowels.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class NumberOfVowels {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String s = sc.nextLine();
 
 
     }
