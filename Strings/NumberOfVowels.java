65a5003f12c919a78ab9a22adf07aac1d2f6ef9a
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-25 09:53:27 +0530

    select char

--- Strings/NumberOfVowels.java
+++ Strings/NumberOfVowels.java
@@ -6,7 +6,12 @@ public class NumberOfVowels {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string: ");
         String s = sc.nextLine();
+        int count = 0;
+        for(int i = 0;i < s.length(); i++){
+            char alf = s.charAt(i);
 
+
+        }
 
     }
 }
