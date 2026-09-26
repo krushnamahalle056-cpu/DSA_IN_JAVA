8f0f559f041fd66a26cc01a7c4108e4883751406
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-26 18:30:47 +0530

    about compareTo string method

---
+++ Strings/CompareToStringMethods.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class CompareToStringMethods {
+    static void main(String[] args) {
+        String a = "Krushna";
+        String b = "Hariom";
+        System.out.println(a.compareTo(b));
+    }
+}
