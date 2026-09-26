6c7b5a3b384a868d00d43d82d4d2e2a719c35299
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-26 18:11:57 +0530

    use compareTo method on string

--- Strings/BuiltInMethods.java
+++ Strings/BuiltInMethods.java
@@ -5,5 +5,10 @@ public class BuiltInMethods {
         String str = "afDEEIOAWEkdfgheriKDDFV";
         System.out.println(str.toUpperCase());
         System.out.println(str.toLowerCase());
+
+        String s = "Harshika";
+        System.out.println(s.compareTo("Harsh"));     // compareTo method give the answer in the form of true or false
+        System.out.println(s.compareTo("ishika"));
+
     }
 }
