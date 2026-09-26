dbf2efe684e389a6916bd4e3248421bdfa669cb3
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-26 10:27:56 +0530

    print my name

--- Strings/PassingStringsToMethod.java
+++ Strings/PassingStringsToMethod.java
@@ -2,6 +2,9 @@ package Strings;
 
 public class PassingStringsToMethod {
     static void main(String[] args) {
+        String MyName = "Krushna";
+        System.out.println("Hello, " + MyName);
+
 
     }
 }
