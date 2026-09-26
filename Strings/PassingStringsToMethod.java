c7525ee7546bafcd5423f641e679f6a5c04c03fc
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-26 10:29:24 +0530

    change funtion add

--- Strings/PassingStringsToMethod.java
+++ Strings/PassingStringsToMethod.java
@@ -1,6 +1,10 @@
 package Strings;
 
 public class PassingStringsToMethod {
+    public static void change(){
+        String x = "Sahil";
+        System.out.println(x);
+    }
     static void main(String[] args) {
         String MyName = "Krushna";
         System.out.println("Hello, " + MyName);
